e61c716d41a5f43ea2ec6f27d1ad27c63431859b
Waleed Saleem <94320646+waleedboss7@users.noreply.github.com>
2022-10-18 12:09:56 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let logo = document.querySelector('.container');
+let menu = document.querySelector('.menu');
+
+logo.addEventListener('click',function() {
+    menu.classList.toggle('showmenu');
+})
+
+
